b388a00345d71ece328fcaf9fa24e2d69e8c1b79
J-Puls <54559578+J-Puls@users.noreply.github.com>
2019-10-17 15:21:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ $(document).ready(function () {
         
         $(".hero").toggleClass("menu-active");
     });
-
 });
+function resizeElements(x) {
+    if (x.matches) { // If media query matches
+        $('.select').addClass('is-large')
+    } else {
+        $('.select').removeClass('is-large')
+    }
+}
 
+var x = window.matchMedia("(min-width: 1024px)")
+resizeElements(x) // Call listener function at run time
+x.addListener(resizeElements) // Attach listener function on state changes
