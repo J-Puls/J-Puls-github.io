6ecd58ff570121c2cfd02738e23e675fe2d28257
J-Puls <54559578+J-Puls@users.noreply.github.com>
2019-10-19 00:16:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ $(document).ready(function () {
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-        
-        $(".hero").toggleClass("menu-active");
+        if ($('.navbar-end').is(':hidden')) {
+            $('.navbar-end').slideDown(200);
+        } else {
+            $('.navbar-end').hide(200);
+            $('.navbar-end').fadeOut(200);
+        }
+
     });
+    $('.card-footer-item').hover(function () {
+        $(this).toggleClass('has-background-black')
+    })
 });
+
 function resizeElements(x) {
     if (x.matches) { // If media query matches
         $('.select').addClass('is-large')
+        $('.navbar-end').show();
     } else {
         $('.select').removeClass('is-large')
     }
